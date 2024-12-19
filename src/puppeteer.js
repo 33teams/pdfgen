df26488014bc511c14ef9dface56cb68b038251c
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2024-12-19 12:12:07 +0000

    Load Paged.js into Puppeteer manually

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -21,18 +21,31 @@ export async function renderText(text) {
  */
 export async function renderUrl(url) {
 	return getPdfData(async (page) => {
-		await page.goto(url.toString());
+		await page.goto(url.toString(), WAIT_FOR);
 	});
 }
 
 /**
  * @param {(page: import("puppeteer").Page) => Promise<void>} loadContent
+ * @param {import("puppeteer").PDFOptions=} options
  * @returns {Promise<Uint8Array>}
  */
-async function getPdfData(loadContent) {
+async function getPdfData(loadContent, options) {
 	const browser = await puppeteer.launch({ dumpio: true });
 	const page = await browser.newPage();
 	page.on("console", (message) => console[message.type()]("[BROWSER]", message.text()));
 	await loadContent(page);
-	return page.pdf().finally(() => browser.close());
+	await page.evaluate(() => {
+		window.__pagedjs_render_complete__ = false;
+		window.PagedConfig = {
+			auto: true,
+			after() {
+				console.debug("render complete");
+				window.__pagedjs_render_complete__ = true;
+			},
+		};
+	})
+	await page.addScriptTag({ url: "https://unpkg.com/pagedjs@0.4.3/dist/paged.polyfill.min.js" });
+	await page.waitForFunction(() => window.__pagedjs_render_complete__ === true)
+	return page.pdf({ ...options, preferCSSPageSize: true }).finally(() => browser.close());
 }
